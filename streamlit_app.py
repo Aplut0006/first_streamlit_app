c8858a5338d0abf0870d5e426d2927f458c65899
Aplut0006 <33050393+Aplut0006@users.noreply.github.com>
2022-09-11 22:22:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('My name is Aplut')
 streamlit.title('chennai is okay')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
