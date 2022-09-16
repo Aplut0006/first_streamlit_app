9b1c23eaaeece0fe60311054a9923483c604ab5f
Aplut0006 <33050393+Aplut0006@users.noreply.github.com>
2022-09-16 23:26:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-
+import snowflake.connector
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+
 
 streamlit.title('My name is Aplut')
 streamlit.title('chennai is okay')
