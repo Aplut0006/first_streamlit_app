8001a2971c0a80635117fafe5bf732abeea6af06
Aplut0006 <33050393+Aplut0006@users.noreply.github.com>
2022-09-17 00:18:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def get_fruitvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-import snowflake.connector
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -58,6 +58,7 @@ streamlit.stop()
 
 
 streamlit.header("This Fruit load list contains:")
+import snowflake.connector
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
